b2c1b5978ea09bd537c054fdf1be3008232e385d
Jianyu Gong <jianyu0503@gmail.com>
2019-06-30 21:13:54 -0400

    Update etl.py
    created song_table, artist table and users table

--- etl.py
+++ etl.py
@@ -2,6 +2,7 @@ import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, LongType, TimestampType
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
@@ -9,8 +10,8 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -23,39 +24,85 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = input_data + "song-data/*/*/*/*.json"
+    
+    # define table schema
+    songdata_schema = StructType([
+        StructField("artist_id", StringType(), True),
+        StructField("artist_latitude", DoubleType(), True),
+        StructField("artist_location", StringType(), True),
+        StructField("artist_longitude", DoubleType(), True),
+        StructField("artist_name", StringType(), True),
+        StructField("duration", DoubleType(), True),
+        StructField("num_songs", IntegralType(), True),     
+        StructField("song_id", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("year", IntegerType(), True), 
+    ])
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data, schema=songdata_schema)
+    
+    # create a temporary table
+    df.createOrReplaceTempView("songs")
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = spark.sql(''' SELECT song_id, artist_id, year, duration
+                                FROM songs
+                            ''')
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.partitionBy('year', 'artist_id').parquet(output_data + "songs")
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = spark.sql(''' SELECT artist_id, artist_name, artist_location, artist_lattitude, artist_longitude
+                                  FROM songs
+                              ''')
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(output_data + "artists")
 
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data =
+    log_data = input_data + "log-data"
+    
+    # define table schema
+    logdata_schema = StructType([
+        StructField("artist", StringType(), True),
+        StructField("auth", StringType(), True),
+        StructField("firstName", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("itemInSession", LongType(), True),
+        StructField("lastName", StringType(), True),
+        StructField("length", DoubleType(), True),
+        StructField("level", StringType(), True),
+        StructField("location", StringType(), True),
+        StructField("method", StringType(), True),
+        StructField("page", StringType(), True),
+        StructField("registration", DoubleType(), True),
+        StructField("sessionId", LongType(), True),
+        StructField("song", StringType(), True),
+        StructField("status", LongType(), True),
+        StructField("ts", TimestampType(), True),
+        StructField("userAgent", StringType(), True),
+        StructField("userId", IntegerType(), True),
+    ])
 
     # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
+    df = spark.read.json(log_data, schema=logdata_schema)
 
+    # create a temporary table
+    df.createOrReplaceTempView("logs")
+    
     # extract columns for users table    
-    artists_table = 
+    users_table = spark.sql(''' SELECT distinct userId as user_id, firstName as first_name,\
+                                       lastName as last_name, gender, level
+                                FROM logs 
+                            ''')
     
     # write users table to parquet files
-    artists_table
+    users_table.write.parquet(output_data + "users")
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
